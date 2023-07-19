ae0bf7067e28b5247905bb9c5d67b248211f56a4
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2023-07-20 02:29:24 +0700

    second commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 # res = pyautogui.locateCenterOnScreen("image.png")
 # print(res)
 
+
 channel_name = pyautogui.prompt(text="",title="Enter the Channel Name")
 print(channel_name)
 pyautogui.click()
@@ -16,13 +17,12 @@ pyautogui.hotkey("ctrl","t")
 
 pyautogui.write("https://www.youtube.com/")
 pyautogui.hotkey("enter")
+
 time.sleep(2)
 x, y = pyautogui.locateCenterOnScreen("youtube_search.png", confidence=0.9)
 pyautogui.moveTo(x,y,1)
 pyautogui.click()
 time.sleep(2)
-
-
 
 pyautogui.write(channel_name)
 pyautogui.hotkey("enter")
